efc2b769f464552d11051030de8432a5f1506eda
KAUSHIK1205 <chezhiworld@gmail.com>
2024-09-13 18:36:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+K='2013-05-12'
+A='12-06-1888'
+U='13-01-1884'
+print(K)
+print(A)
+print(U)
+
+CODE="THESE ARE SOME OF THE BIRTHDAY DATES OF MY FRIENDS AND FAMILY"
+print(CODE)
+name=input ("enter your name please")
+print("\n thanks for visitig my project",name,"\n I hope you like this project")
+print("RAHUL >_<")
+print("once again thank you ")
+
+
+
